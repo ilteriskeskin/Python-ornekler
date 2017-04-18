761327eccb588d1c0f9dd44bc301e073731a9f8c
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2017-04-18 12:21:51 +0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,21 @@
+      kullaniciAdi = input('Kullanici adinizi giriniz: ') #kullanici adi girisini aldik.
+      parola       = input('Parolanizi giriniz       : ') #parola girisini aldik.
+
+      toplamUzunluk = len(kullaniciAdi) + len(parola) #len fonksiyonu ile karakter sayisini ogrendik.
+
+      mesaj = 'Kullanici adi ve parolaniz toplam {} karakterden olusuyor.' #mesajimizi yazdirdik.
+
+      print(mesaj.format(toplamUzunluk)) #format fonksiyonu ile toplam karakter sayisini yazdirdik.
+
+      if toplamUzunluk > 25: #toplam karakter sayisinin 25 ten kucuk olma kosulunu koyduk.
+          print('Kullanici adiniz ve parolaniz toplam 25 karakteri gecmemeli')
+      else:
+          print('Sisteme kaydiniz alinmistir. Lütfen Giris yapiniz.')
+
+      kullaniciAdi1 = input('Kullanici Adi: ')<br>
+      parola1       = input('Parola       : ')
+      
+      if kullaniciAdi1 == kullaniciAdi and parola1 == parola: #and parametresi ile kullanici adi ve
+          print('Sisteme girisiniz basarili!') 		    #parolanin dogrulugunu test ettik.
+      else:
+          print('Kullanici Adi veya Parola Hatali')
