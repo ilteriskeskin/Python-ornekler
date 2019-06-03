4fe1d96f2b94d715cbc45883680d9a5c3bb06286
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2019-06-03 16:38:56 +0300

    yatay-dikey layout örneği eklendi

---
+++ PyQT5/horizontal_vertical_box_layout.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5 import QtWidgets
+
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+
+    okay = QtWidgets.QPushButton('Tamam')
+    cancel = QtWidgets.QPushButton('İptal')
+
+    h_box = QtWidgets.QHBoxLayout()
+    h_box.addWidget(okay)
+    h_box.addWidget(cancel)
+
+    v_box = QtWidgets.QVBoxLayout()
+    v_box.addStretch()
+    v_box.addLayout(h_box)
+
+    pencere = QtWidgets.QWidget()
+    pencere.setWindowTitle('PyQt5 Ders 3')
+
+    pencere.setLayout(v_box)
+
+    pencere.setGeometry(100, 100, 500, 500)
+    pencere.show()
+    sys.exit(app.exec_())
+
+
+window()
