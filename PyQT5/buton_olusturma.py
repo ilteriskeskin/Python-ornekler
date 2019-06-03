2a3a69d69d1c97d552a82a9e0ee821e1ef657d9f
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2019-06-03 16:19:16 +0300

    Buton oluşturuldu

---
+++ PyQT5/buton_olusturma.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5 import QtWidgets, QtGui
+
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    pencere = QtWidgets.QWidget()
+
+    pencere.setWindowTitle('PyQt Ders 1')
+    button = QtWidgets.QPushButton(pencere)
+    button.setText('Bir Buton')
+    etiket = QtWidgets.QLabel(pencere)
+    etiket.setText('Merhaba PyQt5')
+
+    etiket.move(200, 30)
+    button.move(200, 60)
+    pencere.setGeometry(100, 100, 500, 500)
+
+    pencere.show()
+    sys.exit(app.exec_())
+
+
+window()
