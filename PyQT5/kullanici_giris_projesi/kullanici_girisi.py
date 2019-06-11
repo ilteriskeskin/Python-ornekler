ee6f18b8b16588e91aceeff5bf5a713d9a891258
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2019-06-11 11:51:57 +0300

    Kullanıcı girişi projesi eklendi

---
+++ PyQT5/kullanici_giris_projesi/kullanici_girisi.py
@@ -0,0 +1,54 @@
+import sys
+from PyQt5 import QtWidgets
+import sqlite3
+
+class Pencere(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.baglanti_olustur()
+        self.init_ui()
+
+    def baglanti_olustur(self):
+        baglanti = sqlite3.connect('data.db')
+        self.cursor = baglanti.cursor()
+        self.cursor.execute('Create Table If not exists uyeler (kullanici_adi TEXT, parola TEXT)')
+        baglanti.commit()
+
+    def init_ui(self):
+        self.kullanici_adi = QtWidgets.QLineEdit()
+        self.parola = QtWidgets.QLineEdit()
+        self.parola.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.giris = QtWidgets.QPushButton('Giriş Yap')
+        self.yazi_alani = QtWidgets.QLabel('')
+
+        v_box = QtWidgets.QVBoxLayout()
+        v_box.addWidget(self.kullanici_adi)
+        v_box.addWidget(self.parola)
+        v_box.addWidget(self.yazi_alani)
+        v_box.addStretch()
+        v_box.addWidget(self.giris)
+
+        h_box = QtWidgets.QHBoxLayout()
+        h_box.addStretch()
+        h_box.addLayout(v_box)
+        h_box.addStretch()
+
+        self.setLayout(h_box)
+        self.setWindowTitle('Kullanıcı Girişi')
+        self.giris.clicked.connect(self.login)
+        self.show()
+
+    def login(self):
+        adi = self.kullanici_adi.text()
+        par = self.parola.text()
+        self.cursor.execute('Select * From uyeler where kullanici_adi = ? and parola = ?', (adi, par))
+        data = self.cursor.fetchall()
+        if len(data) == 0:
+            self.yazi_alani.setText('Böyle bir kullanıcı yok\nLütfen tekrar deneyin.')
+        else:
+            self.yazi_alani.setText('Giriş işlemi başarılı.\nHoşgeldiniz ' + adi)
+
+app = QtWidgets.QApplication(sys.argv)
+pencere = Pencere()
+sys.exit(app.exec_())
